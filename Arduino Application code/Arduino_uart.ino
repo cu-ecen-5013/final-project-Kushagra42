7321f3d76fedde8ecd9c68ef96ee7c872408bfcb
Kushagra Pandey <kushagra.pandey@colorado.edu>
2020-04-18 21:07:52 -0600

    Arduino loop back test

--- Arduino Application code/Arduino_uart.ino
+++ Arduino Application code/Arduino_uart.ino
@@ -1,54 +1,17 @@
-#include <SoftwareSerial.h>
+// Arduino echo program, written by Derek Molloy to test the BBB UART4
 
-SoftwareSerial mySerial(8, 7); // RX, TX
-char inChar;
-const int temperaturePin = 0;
-
-void setup()
-{
- 
-  Serial.begin(9600);
-  // Open serial communications and wait for port to open:
- 
-  Serial.println("Awaiting command from BBB...");
-  // set the data rate for the SoftwareSerial port
-  mySerial.begin(9600);
-  pinMode(13, OUTPUT);
-  
+// This function is called once when the program starts. 
+void setup() {
+   // Choose a baud rate and configuration. 115200
+   // Default is 8-bit with No parity and 1 stop bit
+   Serial.begin(115200, SERIAL_8N1);
 }
 
-void temp_read()
-{
-  float voltage, degreesC, degreesF;
-  voltage = getVoltage(temperaturePin);
-  degreesC = (voltage - 0.5) * 100.0;  
-  degreesF = degreesC * (9.0/5.0) + 32.0;
-  Serial.print("voltage: ");
-  Serial.print(voltage);
-  Serial.print("  deg C: ");
-  Serial.print(degreesC);
-  Serial.print("  deg F: ");
-  Serial.println(degreesF); 
-  }
-
-float getVoltage(int pin)
-{
-    return (analogRead(pin) * 0.004882814);
-}
-
-void loop() // run over and over
-{
-  if (mySerial.available()){
-    inChar = (char)mySerial.read();
-    if(inChar=='1'){
-      Serial.print("Received read and transmit instructions from BBB ");
-      digitalWrite(13, HIGH);
-      temp_read();
-     }else if(inChar =='0'){
-      Serial.print("Awaitng read and transmit instruction from BBB");    
-      digitalWrite(13, LOW);
-    }else{
-      Serial.print("Unknown instructions ");    
+// This function will loop as quickly as possible, forever.
+void loop() {
+   byte charIn;
+   if(Serial.available()){          // A byte has been received
+      charIn = Serial.read();       // Read the character in from the BBB
+      Serial.write(charIn);         // Send the character back to the BBB
    }
-  }
 }

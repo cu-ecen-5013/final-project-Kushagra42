ab459310ded7e50794feb8863d3b87a50c782a7d
Kushagra Pandey <kushagra.pandey@colorado.edu>
2020-04-25 05:51:31 -0600

    server test

--- Socket_Client/Client.c
+++ Socket_Client/Client.c
@@ -49,7 +49,7 @@ int main()
     }
    
     servaddr.sin_family = AF_INET; 
-    servaddr.sin_addr.s_addr = inet_addr("192.168.0.2"); 
+    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1"); 
     servaddr.sin_port = htons(PORT); 
   
     

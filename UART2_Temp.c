f140ce89ead09f7fc3b7db62fb19d29168f3bf9d
Vaidehi <Vaidehi Salway>
2020-04-25 17:41:31 -0600

    Added all the task in one file

--- UART2_Temp.c
+++ UART2_Temp.c
@@ -3,12 +3,21 @@
 
 #include<stdio.h>
 #include<stdlib.h>
-#include<fcntl.h>
 #include<unistd.h>
 #include<termios.h>   // using the termios.h library
+#include <errno.h>
+#include <string.h>
+#include <signal.h>
 
+#include <linux/i2c.h>
+#include <linux/i2c-dev.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <assert.h>
 #define BUFFER_SIZE 100
-
+#define SLAVE_ADDR 0x40
 //Raspi UART Function
  float ARD_uart()
 {
@@ -114,14 +123,72 @@
    return 0;
 }
 
+float Get_Temperature()
+{
+
+int fd;
+
+void signal_handler(int signum)
+{
+  assert(0 == close(fd));
+  exit(signum);
+}
+
+  char *bus = "/dev/i2c-1"; 
+  int addr = SLAVE_ADDR;    
+  char buf[2] = {0};
+  int temp;
+  unsigned char MSB, LSB;
+
+  float f,c;
+
+  if ((fd = open(bus, O_RDWR)) < 0) {
+    
+    perror("Failed to open the i2c bus");
+    exit(1);
+  }
+
+ if (ioctl(fd, I2C_SLAVE, addr) < 0) {
+   perror("Failed to acquire bus access and/or talk to slave.\n");
+  
+   exit(1);
+ }
+
+ signal(SIGINT, signal_handler);
+
+
+     
+     if (read(fd,buf,2) != 2) {
+      
+       perror("Failed to read from the i2c bus.\n");
+
+   } else {
+
+       MSB = buf[0];
+       LSB = buf[1];
+
+       temp = ((MSB << 8) | LSB) >> 4;
+
+       c = temp*0.0625;
+       f = (1.8 * c) + 32;
+
+       printf("Temp Fahrenheit: %f Celsius: %f\n", f, c);
+     }
+     sleep(1);
+
+return f;
+
+}
 int main()
 {
 		 
 	printf("Sending command 1 to BB and Arduino\n");
 	float Temperature1=ARD_uart();
  	float Temperature2=Rasp_uart();
+	float Temperature3=Get_Temperature();
    	
 	printf("Temperature value from Arduino is: %f\n", Temperature1); 
 	printf("Temperature Value from Raspi is: %f\n", Temperature2);
+        printf("Temperature value from Beaglebone is: %f\n", Temperature3);
 
 }

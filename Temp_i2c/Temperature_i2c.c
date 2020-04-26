d3c27e52eff103f4fd9d4387cbd0106e0e2aab88
HARSH1911RATHORE <hara6329@colorado.edu>
2020-04-26 17:37:43 -0600

    temperature on receiving 1

--- Temp_i2c/Temperature_i2c.c
+++ Temp_i2c/Temperature_i2c.c
@@ -60,52 +60,53 @@ void uart_write( char tx_buffer)
 	        //send the string
       		printf("Failed to write to the output\n");
       		
-   	}
+   	  }
 	printf("Write successful\n");
-;    
+   
 	close(file);
 
 }
 int uart_read()
 {
-  int file,count;
+	int file,count;
 /********************************************open file**********************************/
-   if ((file = open("/dev/ttyAMA0", O_RDWR | O_NOCTTY | O_NDELAY))<0)
-   {
-      perror("UART: Failed to open the file.\n");
-      return -1;
-   }
-   struct termios options;               //The termios structure is vital
-   tcgetattr(file, &options);            //Sets the parameters associated with file
+   	if ((file = open("/dev/ttyAMA0", O_RDWR | O_NOCTTY | O_NDELAY))<0)
+   	{
+		perror("UART: Failed to open the file.\n");
+      		return -1;
+   	}
+   	struct termios options;               //The termios structure is vital
+   	tcgetattr(file, &options);            //Sets the parameters associated with file
 
    // Set up the communications options:
    //   115200 baud, 8-bit, enable receiver, no modem control lines
-   options.c_cflag = B115200 | CS8 | CREAD | CLOCAL;
-   options.c_iflag = IGNPAR | ICRNL; 
-   options.c_oflag = 0;
-   options.c_lflag = 0;
+  	 options.c_cflag = B115200 | CS8 | CREAD | CLOCAL;
+   	options.c_iflag = IGNPAR | ICRNL; 
+   	options.c_oflag = 0;
+   	options.c_lflag = 0;
    //ignore partity errors, CR -> newline
-   tcflush(file, TCIFLUSH);             //discard file information not transmitted
-   tcsetattr(file, TCSANOW, &options);  //changes occur immmediately
+   	tcflush(file, TCIFLUSH);             //discard file information not transmitted
+   	tcsetattr(file, TCSANOW, &options);  //changes occur immmediately
 
-   fcntl(file, F_SETFL, 0);
-   unsigned char receive[BUFFER_SIZE];      //declare a buffer for receiving data
-   if ((count = read(file, (void*)receive, 100))<0){   //receive the datam
-      perror("Failed to read from the input\n");
-      return -1;
-   }
-   if (count==0)
-   { 
-        printf("There was no data available to read!\n");
-        return 0;
-   } 
-   else 
-   {
+   	fcntl(file, F_SETFL, 0);
+   	unsigned char receive[BUFFER_SIZE];      //declare a buffer for receiving data
+   	if ((count = read(file, (void*)receive, 100))<0)      //receive the datam
+	{   
+      		perror("Failed to read from the input\n");
+      		return -1;
+   	}
+   	if (count==0)
+   	{ 
+        	printf("There was no data available to read!\n");
+        	return 0;
+  	 } 
+   	else 
+   	{
      
-      printf("The following sensor value was read in [%d]: %s\n",count,receive);
-      close(file);
-      return 1;
-   }
+      		printf("The following sensor value was read in [%d]: %s\n",count,receive);
+      		close(file);
+      		return 1;
+   	}
  
   
 }

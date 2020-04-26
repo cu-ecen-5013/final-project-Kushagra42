aaeaa79fb8f3a6fa06acfcd6d6da154248f113e9
Kushagra Pandey <kushagra.pandey@colorado.edu>
2020-04-26 15:08:16 -0600

    Client code working

--- Socket_Client/Client.c
+++ Socket_Client/Client.c
@@ -7,63 +7,57 @@
     #include <netinet/in.h> 
     #include <sys/socket.h> 
     #include <unistd.h>
-
+    #include <arpa/inet.h>
     #define PORT 9000    /* the port client will be connecting to */
 
     #define MAXDATASIZE 100 /* max number of bytes we can get at once */
+    
+
+
+    int new_socket;
+
+void Socket_Init()
+{
+	char target_ip[20] = "192.168.50.104";
+	uint32_t Port_Num = 9000;
+	struct sockaddr_in client;
+
+	
+	new_socket = socket(AF_INET, SOCK_STREAM | SOCK_CLOEXEC, 0);
+	if(new_socket < 0)		printf("Socket Creation Failed\n");
+	
+	client.sin_family = AF_INET;
+	if(inet_pton(AF_INET, target_ip, &client.sin_addr) <= 0)	printf("Error in IP Address\n");
+	client.sin_port = htons(Port_Num);
+	
+	if(connect(new_socket, (struct sockaddr *)&client, sizeof(client)) == 0)	printf("Connect Succeeded\n");
+	else	printf("Connect Failed\n");
+	
+	//send data
+
+}
+int Client_Data(char *str, uint32_t len)
+{
+
+   
+    if (send(new_socket, str, len, 0) == -1)
+     {
+         perror("send");
+         exit (1);
+     }
+     printf("After the send function \n");
+
+     return 0;
+   
+}
 
     int main(int argc, char *argv[])
     {
-        int sockfd,numbytes;  
-        char buf[MAXDATASIZE];
-        struct hostent *he;
-        struct sockaddr_in their_addr; /* connector's address information */
+        Socket_Init();
+	while(1)
+	{
+		Client_Data("Testing client-server connection",100);
 
-        if (argc != 2) {
-            fprintf(stderr,"usage: client hostname\n");
-            exit(1);
-        }
-
-        if ((he=gethostbyname(argv[1])) == NULL) {  /* get the host info */
-            herror("gethostbyname");
-            exit(1);
-        }
-
-        if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-            perror("socket");
-            exit(1);
-        }
-
-        their_addr.sin_family = AF_INET;      /* host byte order */
-        their_addr.sin_port = htons(PORT);    /* short, network byte order */
-        their_addr.sin_addr = *((struct in_addr *)he->h_addr);
-        bzero(&(their_addr.sin_zero), 8);     /* zero the rest of the struct */
-
-        if (connect(sockfd, (struct sockaddr *)&their_addr, \
-                                              sizeof(struct sockaddr)) == -1) {
-            perror("connect");
-            exit(1);
-        }
-	//while (1) {
-		if (send(sockfd, "Hello, world!\n", 14, 0) == -1){
-                      perror("send");
-		      exit (1);
-		}
-		printf("After the send function \n");
-
-        	if ((numbytes=recv(sockfd, buf, MAXDATASIZE, 0)) == -1) {
-            		perror("recv");
-            		exit(1);
-		}	
-
-	        buf[numbytes] = '\0';
-
-        	printf("Received in pid=%d, text=: %s \n",getpid(), buf);
-		//sleep(1);
-
-	//}
-
-        close(sockfd);
-
+	}
         return 0;
     }
